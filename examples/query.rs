291e5f68a648854c64de20a080b92f8579bb9731
Richo Healey <richo@psych0tik.net>
2024-02-18 19:37:04 -0800

    Use the new APIs

--- examples/query.rs
+++ examples/query.rs
@@ -6,6 +6,22 @@ async fn main() {
     let token = env::args().nth(1).unwrap();
     let gateway = env::args().nth(2).unwrap();
     let client = franklin_wh::Client::new(&token, &gateway);
-    println!("SoC: {}", client.get_state_of_charge().await.unwrap());
-    println!("inner: {:?}", client.get_iot_user_runtime_datalog().await.unwrap());
+    let inner = client.get_iot_user_runtime_datalog().await.unwrap().inner().unwrap();
+
+    println!(r#"
+Status:
+
+charge: {}
+
+battery: {}
+home_load: {}
+solar: {}
+"#,
+    inner.state_of_charge(),
+    inner.current_battery_draw() * -1.0,
+    inner.current_home_load(),
+    inner.current_solar_yield()
+    )
+
+
 }
